2995191b9ab9a868ad736677a4a9be8f66e0fea5
yeeun <mlikshoot99@gmail.com>
2024-06-24 17:28:45 +0900

    /

--- E2l3_Project/resources/js/joinmembershipfirebase.js
+++ E2l3_Project/resources/js/joinmembershipfirebase.js
@@ -66,5 +66,34 @@ async function updateDatas(collectionName, docId, updateinfoObj) {
   // const docData = await getDoc(docRef);
   await updateDoc(docRef, updateinfoObj);
 }
-export { db, getDatas, addDatas, deleteDatas, updateDatas };
+
+async function updateScore(collectionName, dataObj) {
+  try {
+    const collect = collection(db, collectionName);
+    const q = query(
+      collect,
+      where("id", "==", dataObj.id),
+      where("type", "==", dataObj.type)
+    );
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      // 문서가 존재하지 않으면 새 문서 추가
+      await addDoc(collect, dataObj);
+      console.log("New document successfully added!");
+    } else {
+      // 문서가 존재하면 점수 업데이트
+      querySnapshot.forEach(async (doc) => {
+        const docRef = doc.ref;
+        console.log(docRef);
+        await setDoc(docRef, { score: dataObj.score }, { merge: true });
+        console.log("Document successfully updated!");
+      });
+    }
+  } catch (e) {
+    console.error("Error updating document: ", e);
+  }
+}
+
+export { db, getDatas, addDatas, deleteDatas, updateDatas, updateScore };
 //export 는 말그대로 내보내서 쓰는것들이니까 deleteDats를 쓴거야.
